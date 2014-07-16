f2ffb23c621c2eb2f6033b43a738fcb09aa0a1b2
Hazarapet <hazarapet@hazarapet.(none)>
2014-07-16 20:45:33 +0400

    init

--- Resources/public/javascript/yiInput.js
+++ Resources/public/javascript/yiInput.js
@@ -3,13 +3,16 @@
 angular.module("Geo")
         .directive("yiInput",function($parse){
     return {
-        restrict: 'E',
+        restrict: 'A',
         require: '?ngModel',
         link: function (scope, element, attrs) {
             if (attrs.ngModel) {
                 var val = attrs.value || element.text();
                 $parse(attrs.ngModel).assign(scope, val);
             }
+            scope.$watch(attrs.ngModel,function(d){
+                angular.element("#"+attrs.id).val(d);
+            },true);
         }
     };
 });
