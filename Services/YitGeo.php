119c211b2a3ef30357dc119848e8c8ee5ed82b24
andranik <An.corp@mail.ru>
2014-09-12 02:28:22 +0400

    change

--- Services/YitGeo.php
+++ Services/YitGeo.php
@@ -12,8 +12,8 @@ use Symfony\Component\DependencyInjection\Container;
 
 class YitGeo
 {
-//    const GEO_DOMAIN = 'http://dev.geo.yerevan.am/';
-    const GEO_DOMAIN = 'http://geo.loc/app_dev.php/';
+    const GEO_DOMAIN = 'http://dev.geo.yerevan.am/';
+//    const GEO_DOMAIN = 'http://geo.loc/app_dev.php/';
 
     protected $experience;
 
