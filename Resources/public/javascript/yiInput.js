8f733ceefe3e4f58b5b4efe7c63ec0ca7906b488
andranik <an.corp@mail.ru>
2015-02-03 18:15:14 +0400

    Hazo changes

--- Resources/public/javascript/yiInput.js
+++ Resources/public/javascript/yiInput.js
@@ -1,18 +1,18 @@
 'use stricts';
 
 angular.module("Geo")
-        .directive("yiInput",function($parse){
-    return {
-        restrict: 'A',
-        require: '?ngModel',
-        link: function (scope, element, attrs) {
-            if (attrs.ngModel) {
-                var val = attrs.value || element.text();
-                $parse(attrs.ngModel).assign(scope, val);
+    .directive("yiInput",['$parse',function($parse){
+        return {
+            restrict: 'A',
+            require: '?ngModel',
+            link: function (scope, element, attrs) {
+                if (attrs.ngModel) {
+                    var val = attrs.value || element.text();
+                    $parse(attrs.ngModel).assign(scope, val);
+                }
+                scope.$watch(attrs.ngModel,function(d){
+                    angular.element("#"+attrs.id).val(d);
+                },true);
             }
-            scope.$watch(attrs.ngModel,function(d){
-                angular.element("#"+attrs.id).val(d);
-            },true);
-        }
-    };
-});
+        };
+    }]);
