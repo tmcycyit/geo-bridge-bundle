ee8458298254d900fe1520475205946199283d3c
tigranparemuzian <tigranparemuzian@gmail.com>
2015-04-28 11:16:07 +0400

    add address type

---
+++ Form/Type/AddressCreateType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Yit\GeoBridgeBundle\Form\Type;
+
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class AddressCreateType extends AbstractType
+{
+	/**
+	 * The EntityManager is the central access point to ORM functionality.
+	 */
+	protected $entityManager;
+
+	/**
+	 * @param EntityManager $entityManager
+	 */
+	public function __construct(EntityManager $entityManager)
+	{
+		$this->em = $entityManager;
+	}
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+		$dateTime = $this->em->getRepository('YitGeoBridgeBundle:Address')->getLastUpdate();
+
+        $builder->add('armName', 'text')
+				->add('engName', 'text')
+        		->add('latitude', null)
+        		->add('longitude', null)
+				->add('created', 'hidden', array('mapped' => false,'data'=> $dateTime ))
+				->add('updated', 'hidden', array('mapped' => false,'data'=> $dateTime ));
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Yit\GeoBridgeBundle\Form\Type\AddressCreateType'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'geo_address_create';
+    }
+}
