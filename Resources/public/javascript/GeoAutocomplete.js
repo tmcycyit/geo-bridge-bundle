95ab55826ecdc42389d8183670ff866074b28641
tigranparemuzian <tigranparemuzian@gmail.com>
2015-06-10 16:15:30 +0400

    add admin with template js

--- Resources/public/javascript/GeoAutocomplete.js
+++ Resources/public/javascript/GeoAutocomplete.js
@@ -2,7 +2,7 @@
 
 angular.module("GeoAutocomplete",['YitAutocomplete','Geo'])
     .run([function(){
-        var element = angular.element('div["ng-app"]="AGM"');
+        var element = angular.element('div[ng-app="AGM"]');
         if(angular.isDefined(element) && element) {
             angular.bootstrap(element[0],['AGM']);
         }
--- Resources/public/javascript/GeoMapAutocomplete.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module("GeoMapAutocomplete",['YitAutocomplete','Geo','YitMap','AGM'])
-    .run([function(){
-        var element = angular.element('div[ng-app="AGM"]');
-        if(angular.isDefined(element) && element) {
-            angular.bootstrap(element[0],['AGM']);
-        }
-    }]);
