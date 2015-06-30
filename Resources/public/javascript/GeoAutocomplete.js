683d752b74de28fdbbcf2b71a1446b62f09c60db
tigranparemuzian <tigranparemuzian@gmail.com>
2015-06-30 11:29:40 +0400

    add js change

--- Resources/public/javascript/GeoAutocomplete.js
+++ Resources/public/javascript/GeoAutocomplete.js
@@ -3,7 +3,7 @@
 angular.module("GeoAutocomplete",['YitAutocomplete','Geo'])
     .run([function(){
         var element = angular.element('div[ng-app="AGM"]');
-        if(angular.isDefined(element) && element) {
+        if(angular.isDefined(element) && element && element.length) {
             angular.bootstrap(element[0],['AGM']);
         }
     }]);
